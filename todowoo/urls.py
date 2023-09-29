fbf659fea43e11cbc3310e4b750e529c1041822e
mzokovvladimir <105171108+mzokovvladimir@users.noreply.github.com>
2023-09-29 11:39:21 +0300

    Update urls.py
    Added type instruction

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from todo import views
+from typing import List
 
-urlpatterns = [
+urlpatterns: List[path] = [
     path('admin/', admin.site.urls),
 
     # Auth
